834f82a1adee8ad75bcd57b3575f9963b73b194e
yifan1003 <hyifan9988@gmail.com>
2021-07-10 18:17:37 -0700

    Complete admin and store and product part

---
+++ store/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Product
+# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'price', 'stock', 'category', 'created_date', 'modifiled_date', 'is_available')
+    prepopulated_fields = {'slug': ('product_name', )}
+admin.site.register(Product,ProductAdmin)
