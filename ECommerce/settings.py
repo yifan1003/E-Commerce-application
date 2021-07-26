3a1388478fae3679068b3e44e3b736e9d6cbd313
yifan1003 <hyifan9988@gmail.com>
2021-07-25 21:08:44 -0700

    fix local error

--- ECommerce/settings.py
+++ ECommerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool) # Boolean type
 
-ALLOWED_HOSTS = [ 'e-commerce-env.eba-jdeeicif.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = [ 'e-commerce-env.eba-jdeeicif.us-west-2.elasticbeanstalk.com','127.0.0.1']
 
 
 # Application definition
